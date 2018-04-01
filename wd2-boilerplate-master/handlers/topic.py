5b0336711330390cb572cfe03da80136fdfa0552
Mateja Vrecic <vrecic.mateja@gmail.com>
2018-04-01 13:24:56 +0200

    Drugi commit

--- wd2-boilerplate-master/handlers/topic.py
+++ wd2-boilerplate-master/handlers/topic.py
@@ -1,14 +1,27 @@
 from handlers.base import BaseHandler
 from models.topic import Topic
+from models.comment import Comment
 from google.appengine.api import users
+from google.appengine.api import memcache
+import uuid
 class TopicHandler(BaseHandler):
     def get(self):
-        return self.render_template("topic_add.html")
+        csrf_token =str(uuid.uuid4())
+        user=users.get_current_user()
+        memcache.add(key=user.email(), value=csrf_token, time=600)
+
+        params = {"token": csrf_token}
+        return self.render_template("topic_add.html", params=params)
+
     def post(self):
         user = users.get_current_user()
+        csrf_token = self.request.get("csrf_token")
+        token = memcache.get(key=user.email())
         if not user:
             return self.write("You have to login before you can post a topic!")
 
+        if token != csrf_token:
+            return self.write("You are hacker!")
         title = self.request.get("title")
         content = self.request.get("text")
 
@@ -19,6 +32,10 @@ class TopicHandler(BaseHandler):
 
 class TopicDetailHandler(BaseHandler):
     def get(self, topic_id):
+        user=users.get_current_user()
         topic = Topic.get_by_id(int(topic_id))
-        params={"topic": topic}
+        comment=Comment.query(Comment.topic_id==topic.key.id(), Comment.deleted==False).order(Comment.created).fetch()
+        csrf_token=str(uuid.uuid4())
+        memcache.add(key=user.email(), value=csrf_token, time=600)
+        params={"topic": topic, "comments": comment, "csrf_token": csrf_token}
         return self.render_template("topic_details.html", params=params)
